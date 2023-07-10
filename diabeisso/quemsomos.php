30c215fd4ea342ade7de0df8119280f2d0c9d4c3
felipppe2005 <113396587+felipppe2005@users.noreply.github.com>
2023-07-10 11:51:06 -0300

    ajusdtes na parte do menu e o body no quem somos

--- diabeisso/quemsomos.php
+++ diabeisso/quemsomos.php
@@ -5,20 +5,33 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Quem somos?</title>
 	<link rel="stylesheet" href="assets/css/estiloqs.css">
-	  <nav id="menu">
-        <ul>
+	  
+	
+</head>
+<body>
+<header>
+        <nav id="menu">
+                 
+                   <img src="assets/img/aaa.png" alt="" srcset="">
+        <ul class="nav-list">    
             <li><a href="index.php">Início</a></li>
             <li><a href="projeto.php">Projeto</a></li>
-            <li><a href="#">Quem somos?</a></li>
+            <li><a href="quemsomos.php">Quem somos?</a></li>
             <li><a href="sugestoes.php">Sugestões</a></li>
             <li><a href="contato.php">Contatos</a></li>
         </ul>
    </nav>
-	
-</head>
-<body>
-   <img src="assets/img/quee.png"width="1895wh" height="895vh" id="imagem">
+</header>
+<div class="div-oxe">
+       
+       </div>
+   
    <p>O Dicionário Diabeisso é um site idealizado e desenvolvido pelos alunos do 3º ano do curso
             técnico em informática.</p> 
+
+
+            <div class="div-oxe1">
+       
+       </div>
 </body>
 </html>
